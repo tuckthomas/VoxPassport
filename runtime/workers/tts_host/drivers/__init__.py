d6d684ab19a2dfaf1252b76e256c2cda501d9797
Tucker Thomas Olson <tuckerolson13@gmail.com>
2026-08-23 02:27:57 -0400

    feat: add TTS driver package

---
+++ runtime/workers/tts_host/drivers/__init__.py
@@ -0,0 +1,5 @@
+"""Built-in TTS worker drivers.
+
+Drivers adapt model libraries/backends to the stable worker-side TtsDriver
+contract. New models should reuse an existing driver whenever possible.
+"""
